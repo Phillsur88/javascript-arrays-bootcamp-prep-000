b0dcfea40d26456a8f3ed3e53daec771b7d6cade
Phillip Surrett <dervareal90@mail.com>
2018-04-24 17:58:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+addElementToBeginningOfArray(array, element)
+const = ([1], "element")
 
-addElementToBeginningOfArray(element, array)
-const array = [1]
